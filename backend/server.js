c34a7cd953218781d3d948949aaa6a7c51243fe6
Trang <dothuytrang2312@gmail.com>
2026-05-12 15:13:50 +0200

    Add POST to backend

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,32 @@ app.get("/", (req, res) => {
   res.send(`"${quote.quote}" -${quote.author}`);
 });
 
+app.post("/", (req, res) => {
+  const bodyBytes = [];
+  req.on("data", chunk => bodyBytes.push(...chunk));
+  req.on("end", () => {
+    const bodyString = String.fromCharCode(...bodyBytes);
+    let body;
+    try {
+      body = JSON.parse(bodyString);
+    } catch (error) {
+      console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
+      res.status(400).send("Expected body to be JSON.");
+      return;
+    }
+    if (typeof body != "object" || !("quote" in body) || !("author" in body)) {
+      console.error(`Failed to extract quote and author from post body: ${bodyString}`);
+      res.status(400).send("Expected body to be a JSON object containing keys quote and author.");
+      return;
+    }
+      quotes.push({
+      quote: body.quote,
+      author: body.author,
+    });
+    res.send("ok");
+  });
+});
+
 app.listen(port, () => {
   console.error(`Quote server listening on port ${port}`);
 });
